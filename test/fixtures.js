24d07bedfef7518495eb3b1311b81d53c7b64a14
Thomas Tuts <thomas.tuts@gmail.com>
2015-06-12 10:45:57 +0200

    Add basic tests for player model

---
+++ test/fixtures.js
@@ -0,0 +1,22 @@
+module.exports = {
+  player: {
+    name: 'Geralt'
+  },
+  deck: {
+    faction: 'monster',
+    cards: [
+      'ghoul',
+      'ghoul',
+      'nekker',
+      'wyvern',
+      'celaeno-harpy',
+      'celaeno-harpy',
+      'endrega',
+      'vampire-garkain',
+      'vampire-garkain',
+      'botchling',
+      'botchling',
+      'botchling'
+    ]
+  }
+};
---
+++ test/models/player.js
@@ -0,0 +1,31 @@
+require('should');
+var fixtures = require('../fixtures');
+var Player = require('../../server/models/player');
+
+describe('Player creation', function () {
+  it('should add a default name if the player gives no name', function () {
+    var player = new Player({
+      deck: fixtures.deck
+    });
+
+    player.should.have.property('name', 'Stranger');
+  });
+
+  it('should add the player name if it is defined', function () {
+    var player = new Player({
+      name: fixtures.player.name,
+      deck: fixtures.deck
+    });
+
+    player.should.have.property('name', fixtures.player.name);
+  });
+
+  it('should add a deck', function () {
+    var player = new Player({
+      name: 'Geralt',
+      deck: fixtures.deck
+    });
+
+    player.should.have.property('deck');
+  });
+});
