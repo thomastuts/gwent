35102d23e608a64afc2fbf293ab12024bfe521fb
Thomas Tuts <thomas.tuts@gmail.com>
2015-06-12 20:43:04 +0200

    Move test game to separate file

--- server/index.js
+++ server/index.js
@@ -1,20 +1,3 @@
 var Board = require('./models/board');
-var presetDeck = require('./preset-deck');
 
-var game = new Board(1);
 
-game.setPlayerOne({
-  name: 'Geralt'
-});
-
-game.playerOne.setDeck(presetDeck);
-game.playerOne.readyUp();
-
-game.setPlayerTwo({
-  name: 'Innkeeper'
-});
-
-game.playerTwo.setDeck(presetDeck);
-game.playerTwo.readyUp();
-
-game.startGame();
