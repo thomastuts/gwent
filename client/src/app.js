6a1b3282bb4866fca1627cc062b1d67720fed155
Thomas Tuts <thomas.tuts@gmail.com>
2015-06-20 16:49:51 +0200

    Add React integration

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,11 @@
+let React = require('react');
+let Lobby = require('./components/lobby');
 let socket = io.connect('http://localhost:9090');
+
+React.render(
+  <Lobby />,
+  document.getElementById('app')
+);
 
 socket.on('news', (data) => {
   console.log(data);
---
+++ client/src/components/lobby.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+class Lobby extends React.Component {
+  render() {
+    return (
+      <h2>Lobby</h2>
+    );
+  }
+}
+
+export default Lobby;
