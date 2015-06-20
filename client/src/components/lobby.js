ce4a2298ce66110e4a6ad62eb19c211773c95ac2
Thomas Tuts <thomas.tuts@gmail.com>
2015-06-20 16:56:05 +0200

    Stub create/join game actions for Lobby component

--- client/src/components/lobby.js
+++ client/src/components/lobby.js
@@ -1,9 +1,21 @@
 var React = require('react');
 
 class Lobby extends React.Component {
+  createGame() {
+    console.log('Creating game');
+  }
+
+  joinGame() {
+    console.log('Joining game');
+  }
+
   render() {
     return (
-      <h2>Lobby</h2>
+      <div>
+        <h2>Lobby</h2>
+        <button onClick={this.createGame}>Create game</button>
+        <button onClick={this.joinGame}>Join game</button>
+      </div>
     );
   }
 }
